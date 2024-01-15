cb005e9f082603c746978a7978ea1069c7e0bee0
Guga Villar <grsv.21@gmail.com>
2024-01-15 20:36:35 -0300

    :recycle: Refact: ItemMenu component

--- src/components/Menu/ItemMenu.tsx
+++ src/components/Menu/ItemMenu.tsx
@@ -11,6 +11,39 @@ type ItemMenuProps = {
   }>
 }
 
+const ActiveLink = ({ icon, label, url }: ItemMenuProps['itens'][number]) => {
+  return (
+    <Link
+      href={url}
+      className="flex h-14 w-48 items-center gap-4 rounded-md bg-brand-500 bg-opacity-10 p-4"
+    >
+      {icon({ className: 'text-brand-500 h-6 w-6' })}
+      <span className="text-brand-500">{label}</span>
+    </Link>
+  )
+}
+
+const NotActiveLink = ({
+  icon,
+  label,
+  url,
+}: ItemMenuProps['itens'][number]) => {
+  return (
+    <Link
+      href={url}
+      className="group flex h-14 w-48 items-center gap-4 rounded-md p-4 transition-colors hover:bg-brand-500 hover:bg-opacity-10"
+    >
+      {icon({
+        className:
+          'text-zinc-800 h-6 w-6 transition-colors group-hover:text-brand-500',
+      })}
+      <span className="text-zinc-800 transition-colors group-hover:text-brand-500">
+        {label}
+      </span>
+    </Link>
+  )
+}
+
 export const ItemMenu = ({ itens }: ItemMenuProps) => {
   const path = usePathname()
 
@@ -19,28 +52,9 @@ export const ItemMenu = ({ itens }: ItemMenuProps) => {
   return itens.map((item) => {
     const isActive = path === item.url
     return isActive ? (
-      <Link
-        key={item.label}
-        href={item.url}
-        className="flex h-14 w-48 items-center gap-4 rounded-md bg-brand-500 bg-opacity-10 p-4"
-      >
-        {item.icon({ className: 'text-brand-500 h-6 w-6' })}
-        <span className="text-brand-500">{item.label}</span>
-      </Link>
+      <ActiveLink key={item.label} {...item} />
     ) : (
-      <Link
-        key={item.label}
-        href={item.url}
-        className="group flex h-14 w-48 items-center gap-4 rounded-md p-4 transition-colors hover:bg-brand-500 hover:bg-opacity-10"
-      >
-        {item.icon({
-          className:
-            'text-zinc-800 h-6 w-6 transition-colors group-hover:text-brand-500',
-        })}
-        <span className="text-zinc-800 transition-colors group-hover:text-brand-500">
-          {item.label}
-        </span>
-      </Link>
+      <NotActiveLink key={item.label} {...item} />
     )
   })
 }
