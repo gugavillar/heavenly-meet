6ad26ccea85b780b41b20668a4b8e62e7590143b
Guga Villar <grsv.21@gmail.com>
2024-02-03 07:59:06 -0300

    :recycle: Refact with prettier-plugin-tailwindcss

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -9,18 +9,8 @@ export const Card = ({ children, className, ...props }: CardProps) => {
   return (
     <article
       className={twMerge(
-        `
-        overflow-x-auto rounded-xl bg-white 
-        p-5
-        shadow
-        scrollbar
-        scrollbar-track-gray-100
-        scrollbar-thumb-gray-400
-       
-      `,
+        'overflow-x-auto rounded-xl bg-white p-5 shadow scrollbar scrollbar-track-gray-100 scrollbar-thumb-gray-400',
         className,
-        `
-      `,
       )}
       {...props}
     >
